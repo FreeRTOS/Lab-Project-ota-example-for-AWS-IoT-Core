688c1d21ce3b71a4f86cc1ce69bd4232288fda4b
John Hendrickson <johnrhen@amazon.com>
2023-07-12 16:13:28 -0700

    Rename mqtt file downloader component

--- lib/aws-iot-core-mqtt-file-downloader/mqttFileDownloaderFilePaths.cmake
+++ lib/aws-iot-core-mqtt-file-downloader/mqttFileDownloaderFilePaths.cmake
@@ -1,8 +1,8 @@
 
-set( MQTT_FILE_SOURCES
+set( MQTT_FILE_DOWNLOADER_SOURCES
 "${CMAKE_CURRENT_LIST_DIR}/source/MQTTFileDownloader_base64.c"
 "${CMAKE_CURRENT_LIST_DIR}/source/MQTTFileDownloader_cbor.c"
 "${CMAKE_CURRENT_LIST_DIR}/source/MQTTFileDownloader.c"
 )
 
-set( MQTT_FILE_INCLUDES "${CMAKE_CURRENT_LIST_DIR}/source/include")
+set( MQTT_FILE_DOWNLOADER_INCLUDES "${CMAKE_CURRENT_LIST_DIR}/source/include")
