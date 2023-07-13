8f48534fcb4d21573d8aed4a82c88c554fad344b
Kody Stribrny <kstribrn@amazon.com>
2023-07-13 13:15:00 -0700

    Update license header
    Update license file header to follow
    open source licensing guidance.

--- demo/mqtt_wrapper/mqtt_wrapper.h
+++ demo/mqtt_wrapper/mqtt_wrapper.h
@@ -16,9 +16,28 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef CORE_MQTT_API_CONFIG_H
-#define CORE_MQTT_API_CONFIG_H
+#ifndef MQTT_WRAPPER_H
+#define MQTT_WRAPPER_H
 
-#define MQTT_STATE_ARRAY_MAX_COUNT ( 20U )
+#include "core_mqtt.h"
+
+void setCoreMqttContext( MQTTContext_t * mqttContext );
+
+MQTTContext_t * getCoreMqttContext( void );
+
+void setThingName( char * thingName );
+
+void getThingName( char * thingNameBuffer );
+
+bool mqttConnect( char * thingName );
+
+bool isMqttConnected( void );
+
+bool mqttPublish( char * topic,
+                  size_t topicLength,
+                  uint8_t * message,
+                  size_t messageLength );
+
+bool mqttSubscribe( char * topic, size_t topicLength );
 
 #endif
