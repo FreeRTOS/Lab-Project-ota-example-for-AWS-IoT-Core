5521c5b81045c845c123a3fb7b69409b638066e6
Kody Stribrny <kstribrn@amazon.com>
2023-08-07 15:40:08 -0700

    Initial unit test commit

---
+++ lib/iot-core-jobs/test/unit-test/core_jobs_utest.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License. See the LICENSE accompanying this file
+ * for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "unity.h"
+
+#include "core_jobs.h"
+#include "core_json.h"
+
+/* ===========================   UNITY FIXTURES ============================ */
+
+
+/* Called before each test method. */
+void setUp()
+{
+}
+
+/* Called after each test method. */
+void tearDown()
+{
+}
+
+/* Called at the beginning of the whole suite. */
+void suiteSetUp()
+{
+}
+
+/* Called at the end of the whole suite. */
+int suiteTearDown( int numFailures )
+{
+    return numFailures;
+}
+
+/* ===============================   TESTS   =============================== */
+
+void test_blah( void )
+{
+
+}
