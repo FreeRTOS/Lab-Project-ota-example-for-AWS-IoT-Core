978149ed69d4d02096259e84171812b2ff1ec364
Kody Stribrny <kstribrn@dev-dsk-kstribrn-2b-51c8f199.us-west-2.amazon.com>
2023-08-11 20:39:18 +0000

    Remove FreeRTOS kernel from jobs tests
    Remove FreeRTOS-Kernel dependency from jobs
    unit tests. This dependency was only needed
    since the test dependency of CoreMQTT was including
    the header. With the CoreMQTT configuration fixed,
    we no longer need this.

--- lib/iot-core-jobs/test/unit-test/dependencies/coreMQTT/core_mqtt_config.h
+++ lib/iot-core-jobs/test/unit-test/dependencies/coreMQTT/core_mqtt_config.h
@@ -10,35 +10,8 @@
 #ifndef CORE_MQTT_CONFIG_H
 #define CORE_MQTT_CONFIG_H
 
-#include "api_cfg/core_mqtt_config.h"
-
-#include "FreeRTOS.h"
-#include "semphr.h"
-
 #define MQTT_RECV_POLLING_TIMEOUT_MS    ( 1000 )
 #define MQTT_COMMAND_CONTEXTS_POOL_SIZE ( 10 )
-
-extern SemaphoreHandle_t MQTTAgentLock;
-extern SemaphoreHandle_t MQTTStateUpdateLock;
-#define MQTT_PRE_SEND_HOOK( context )                            \
-    if( MQTTAgentLock != NULL )                                  \
-    {                                                            \
-        xSemaphoreTakeRecursive( MQTTAgentLock, portMAX_DELAY ); \
-    }
-#define MQTT_POST_SEND_HOOK( context )            \
-    if( MQTTAgentLock != NULL )                   \
-    {                                             \
-        xSemaphoreGiveRecursive( MQTTAgentLock ); \
-    }
-#define MQTT_PRE_STATE_UPDATE_HOOK( pContext )                \
-    if( MQTTStateUpdateLock != NULL )                         \
-    {                                                         \
-        xSemaphoreTake( MQTTStateUpdateLock, portMAX_DELAY ); \
-    }
-#define MQTT_POST_STATE_UPDATE_HOOK( pContext ) \
-    if( MQTTStateUpdateLock != NULL )           \
-    {                                           \
-        xSemaphoreGive( MQTTStateUpdateLock );  \
-    }
+#define MQTT_STATE_ARRAY_MAX_COUNT ( 20U )
 
 #endif
