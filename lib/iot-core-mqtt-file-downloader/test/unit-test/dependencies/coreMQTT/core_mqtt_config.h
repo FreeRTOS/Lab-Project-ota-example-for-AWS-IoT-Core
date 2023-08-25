72f8795baa856c0761406ba93f625f57d8b8ab22
Kody Stribrny <kstribrn@amazon.com>
2023-08-25 11:02:21 -0700

    Basic unit testing setup
    Sets up necessary CMake files for
    building unit tests for the MQTT
    file downloader componenent.

---
+++ lib/iot-core-mqtt-file-downloader/test/unit-test/dependencies/coreMQTT/core_mqtt_config.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License. See the LICENSE accompanying this file
+ * for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#ifndef CORE_MQTT_CONFIG_H
+#define CORE_MQTT_CONFIG_H
+
+// This header is required by CoreMQTT
+
+#endif
